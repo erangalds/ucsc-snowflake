d326e96747f919724f62281d1ebb50526e22136a
eranga de silva <erangalds@yahoo.com>
2023-10-09 19:11:02 +0530

    validated and commented on LOAD HISTORY lab.

--- stagaes-data-loading-labs/10 Load History.sql
+++ stagaes-data-loading-labs/10 Load History.sql
@@ -3,24 +3,27 @@
 // Query load history within a database
 
 USE COPY_DB;
-
+-- by calling the load_history view we can get the entire load history for the database. 
 SELECT * FROM information_schema.load_history;
 
 // Query load history gloabally from SNOWFLAKE database --
-SELECT * FROM snowflake.account_usage.load_history
-
+select current_role();
+use role accountadmin;
+SELECT * FROM snowflake.account_usage.load_history;
+-- we have to elevate the role to accountadmin in order to query the global load_history details from the snowflake database
 
 // Filter on specific table & schema
 SELECT * FROM snowflake.account_usage.load_history
   where schema_name='PUBLIC' and
   table_name='ORDERS';
   
-// Filter on specific table & schema
+// Filter on specific table & schema and looking for loades with errors
 SELECT * FROM snowflake.account_usage.load_history
   where schema_name='PUBLIC' and
   table_name='ORDERS' and
   error_count > 0;
   
 // Filter on specific table & schema
+-- selecting data loads done during the last 24 hours
 SELECT * FROM snowflake.account_usage.load_history
 WHERE DATE(LAST_LOAD_TIME) <= DATEADD(days,-1,CURRENT_DATE);
