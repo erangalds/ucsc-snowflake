02d216e610d183d59efb4d2a0b61b64586a871cb
eranga de silva <erangalds@yahoo.com>
2023-10-09 19:04:39 +0530

    validated and commented the COPY FORCE Option lab

--- stagaes-data-loading-labs/09 FORCE Option.sql
+++ stagaes-data-loading-labs/09 FORCE Option.sql
@@ -1,4 +1,5 @@
-// Defaut behaviour of Snowflake is such that, it will not load a source file, into a table, if it was loaded previously
+// Defaut behaviour of Snowflake is such that, it will not load a source file, into a table,
+// if it was loaded previously
 // But we can change that behaviour and force snowflake to load previously loaded files. 
 
 CREATE OR REPLACE TABLE  COPY_DB.PUBLIC.ORDERS (
@@ -13,19 +14,19 @@ CREATE OR REPLACE TABLE  COPY_DB.PUBLIC.ORDERS (
 CREATE OR REPLACE STAGE COPY_DB.PUBLIC.aws_stage_copy
     url='s3://snowflakebucket-copyoption/size/';
   
-LIST @COPY_DB.PUBLIC.aws_stage_copy
+LIST @COPY_DB.PUBLIC.aws_stage_copy;
     
 //Load data using copy command
 COPY INTO COPY_DB.PUBLIC.ORDERS
-    FROM @aws_stage_copy
+    FROM @COPY_DB.PUBLIC.aws_stage_copy
     file_format= (type = csv field_delimiter=',' skip_header=1)
-    pattern='.*Order.*'
+    pattern='.*Order.*';
 
 // Not possible to load file that have been loaded and data has not been modified
 COPY INTO COPY_DB.PUBLIC.ORDERS
-    FROM @aws_stage_copy
+    FROM @COPY_DB.PUBLIC.aws_stage_copy
     file_format= (type = csv field_delimiter=',' skip_header=1)
-    pattern='.*Order.*'
+    pattern='.*Order.*';
    
 
 SELECT * FROM ORDERS;    
@@ -33,7 +34,7 @@ SELECT * FROM ORDERS;
 // Using the FORCE option
 
 COPY INTO COPY_DB.PUBLIC.ORDERS
-    FROM @aws_stage_copy
+    FROM @COPY_DB.PUBLIC.aws_stage_copy
     file_format= (type = csv field_delimiter=',' skip_header=1)
     pattern='.*Order.*'
     FORCE = TRUE;
