0c4629294546dabb8938029071a314b6c2b69646
eranga de silva <erangalds@yahoo.com>
2023-10-09 18:59:43 +0530

    validated and commented on TRUNCATECOLUMN Option lab

--- stagaes-data-loading-labs/08 TRUNCATECOLUMN Option.sql
+++ stagaes-data-loading-labs/08 TRUNCATECOLUMN Option.sql
@@ -1,5 +1,14 @@
 // Now let's see how we can handle loading data which are larger than the specified column sizes
 ---- TRUNCATECOLUMNS ----
+select current_role();
+use role sysadmin;
+use database our_first_db;
+select current_database();
+show schemas;
+use schema public;
+select current_schema();
+show tables;
+
 CREATE OR REPLACE TABLE  COPY_DB.PUBLIC.ORDERS (
     ORDER_ID VARCHAR(30),
     AMOUNT VARCHAR(30),
@@ -12,16 +21,20 @@ CREATE OR REPLACE TABLE  COPY_DB.PUBLIC.ORDERS (
 CREATE OR REPLACE STAGE COPY_DB.PUBLIC.aws_stage_copy
     url='s3://snowflakebucket-copyoption/size/';
   
-LIST @COPY_DB.PUBLIC.aws_stage_copy
+LIST @COPY_DB.PUBLIC.aws_stage_copy;
     
  //Load data using copy command
 COPY INTO COPY_DB.PUBLIC.ORDERS
-    FROM @aws_stage_copy
+    FROM @COPY_DB.PUBLIC.aws_stage_copy
     file_format= (type = csv field_delimiter=',' skip_header=1)
-    pattern='.*Order.*'
-
+    pattern='.*Order.*';
+-- Query failes because the column character size is lower than the filed value in the file
+-- In such situations, if needed we can enable TRUNCATECOLUMNS option
+-- That will load part of the value in the field to suite the actual column size
 COPY INTO COPY_DB.PUBLIC.ORDERS
-    FROM @aws_stage_copy
+    FROM @COPY_DB.PUBLIC.aws_stage_copy
     file_format= (type = csv field_delimiter=',' skip_header=1)
     pattern='.*Order.*'
     TRUNCATECOLUMNS = true; 
+-- Validate the data load
+select * from copy_db.public.orders;
