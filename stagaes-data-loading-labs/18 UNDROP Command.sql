5bfa28f8beef510d9d834d598d82ecdd131f6d42
erangalds <erangalds@yahoo.com>
2023-10-10 12:52:38 +0530

    validated and commented on UNDROP command lab.

--- stagaes-data-loading-labs/18 UNDROP Command.sql
+++ stagaes-data-loading-labs/18 UNDROP Command.sql
@@ -8,6 +8,7 @@ CREATE OR REPLACE STAGE MANAGE_DB.external_stages.time_travel_stage
     URL = 's3://data-snowflake-fundamentals/time-travel/'
     file_format = MANAGE_DB.file_formats.csv_file;
     
+drop table if exists our_first_db.public.customers;
 // Create a temporary table
 CREATE OR REPLACE TABLE OUR_FIRST_DB.public.customers (
   id int,
@@ -81,7 +82,7 @@ SELECT * FROM OUR_FIRST_DB.public.customers;
 // Undroping with a name that already exists
 // Making a mistake while restoring (using a wrong query id)
 CREATE OR REPLACE TABLE OUR_FIRST_DB.public.customers as
-SELECT * FROM OUR_FIRST_DB.public.customers before (statement => '01af8c59-0000-7714-0003-df4a0011d7fa');
+SELECT * FROM OUR_FIRST_DB.public.customers before (statement => '01af8caf-0000-77d4-0003-df4a0011e812');
 
 // verifying the 
 SELECT * FROM OUR_FIRST_DB.public.customers;
@@ -91,17 +92,44 @@ SELECT * FROM OUR_FIRST_DB.public.customers;
 UNDROP table OUR_FIRST_DB.public.customers;
 -- But we get an error because the table was dropped and created as new using the create or replace statement earlier
 -- let us rename the table
-drop table if exists our_first_db.public.customers_wrong;
+show tables;
+CREATE OR REPLACE TABLE OUR_FIRST_DB.public.customers (
+  id int,
+  first_name string,
+  last_name string,
+  email string,
+  gender string,
+  job string,
+  phone string);
+    
+// Loading Data
+COPY INTO OUR_FIRST_DB.public.customers
+from @MANAGE_DB.external_stages.time_travel_stage
+files = ('customers.csv');
+--validate data load
+SELECT * FROM OUR_FIRST_DB.public.customers;
+-- Making the Mistake again
+UPDATE OUR_FIRST_DB.public.customers
+SET LAST_NAME = 'Tyson';
+UPDATE OUR_FIRST_DB.public.customers
+SET JOB = 'Data Analyst';
+-- We have make two mistakes now
+SELECT * FROM OUR_FIRST_DB.public.customers;
+-- Renaming the Original Table to with a New Name
+
 ALTER TABLE OUR_FIRST_DB.public.customers
 RENAME TO OUR_FIRST_DB.public.customers_wrong;
 
 show tables;
 DESC table OUR_FIRST_DB.public.customers;
 // Correcting the mistake by using the correct query id
+-- We restore the customer table from the customer_wrong table's time travel details
 CREATE OR REPLACE TABLE OUR_FIRST_DB.public.customers as
-SELECT * FROM OUR_FIRST_DB.public.customers before (statement => '01af8c51-0000-7714-0003-df4a0011d79e');
+SELECT * FROM OUR_FIRST_DB.public.customers_wrong before (statement => '01af8cb5-0000-7714-0003-df4a0011d876');
 
 DESC table OUR_FIRST_DB.public.customers;
 
 SELECT * FROM OUR_FIRST_DB.public.customers;
-    
+--  Therefore its always better to either follow this was by renaming original table and recreating the previous table with the renamed table
+-- Or restore the data to a new table then truncate the original able and re-insert data. 
+-- The reason is as soon as we issue a create or replace the time travel information gets reset.     
