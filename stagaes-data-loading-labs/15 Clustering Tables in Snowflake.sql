65741bbaf0dbc7956a5ff64b3e9a2270a7b87b79
erangalds <erangalds@yahoo.com>
2023-10-10 09:51:52 +0530

    validated and commented on the Clustering Tables in Snowflakes lab

--- stagaes-data-loading-labs/15 Clustering Tables in Snowflake.sql
+++ stagaes-data-loading-labs/15 Clustering Tables in Snowflake.sql
@@ -1,5 +1,8 @@
 // Demonstrating the benefits of using Cluster Keys in tables
 // Publicly accessible staging area    
+use schema our_first_db.public;
+use role sysadmin;
+use warehouse compute_wh;
 CREATE OR REPLACE STAGE MANAGE_DB.external_stages.aws_stage
     url='s3://bucketsnowflakes3';
 
@@ -7,6 +10,11 @@ CREATE OR REPLACE STAGE MANAGE_DB.external_stages.aws_stage
 LIST @MANAGE_DB.external_stages.aws_stage;
 
 //Load data using copy command
+select current_role(), current_database(), current_schema(), current_warehouse();
+show tables;
+-- We have our previously created orders table. Let's use that. 
+truncate table our_first_db.public.orders;
+select * from our_first_db.public.orders;
 COPY INTO OUR_FIRST_DB.PUBLIC.ORDERS
     FROM @MANAGE_DB.external_stages.aws_stage
     file_format= (type = csv field_delimiter=',' skip_header=1)
@@ -15,7 +23,7 @@ COPY INTO OUR_FIRST_DB.PUBLIC.ORDERS
 // Verify Data load
 SELECT * FROM OUR_FIRST_DB.PUBLIC.ORDERS;
 // Create table
-
+-- creating another table to to show caching
 CREATE OR REPLACE TABLE COPY_DB.PUBLIC.ORDERS_CACHING (
 ORDER_ID	VARCHAR(30)
 ,AMOUNT	NUMBER(38,0)
@@ -40,25 +48,65 @@ t1.ORDER_ID
 FROM ORDERS t1
 CROSS JOIN (SELECT * FROM OUR_FIRST_DB.PUBLIC.ORDERS) t2
 CROSS JOIN (SELECT TOP 100 * FROM OUR_FIRST_DB.PUBLIC.ORDERS) t3;
-
+-- loaded 225000000 rows
 
 // Query Performance before Cluster Key
 // Let's query without a WHERE Clause
-SELECT * FROM ORDERS_CACHING;
+SELECT * FROM COPY_DB.PUBLIC.ORDERS_CACHING;
+/*
+Query Duration	25s
+Rows	225,000,000
+End time	Tue Oct 10 2023
+Scanned	1.1GB (100%)
+ */
 // Now Let's query with a WHERE Clause
-SELECT * FROM ORDERS_CACHING  WHERE DATE = '2020-06-09';
-
-SELECT * FROM ORDERS_CACHING  WHERE DATE = '2020-09-15';
-
+SELECT * FROM COPY_DB.PUBLIC.ORDERS_CACHING  WHERE DATE = '2020-06-09';
+/*
+Query Duration	798ms
+Rows	97,049
+End time	Tue Oct 10 2023
+Scanned	1.1GB (100%)
+ */
+SELECT * FROM COPY_DB.PUBLIC.ORDERS_CACHING  WHERE DATE = '2020-09-15';
+/*
+Query Duration	655ms
+Rows	96,546
+End time	Tue Oct 10 2023
+Scanned	1.1GB (100%)
+ */
 // Adding Cluster Key & Compare the result
 
-ALTER TABLE ORDERS_CACHING CLUSTER BY ( DATE );
-
-SELECT * FROM ORDERS_CACHING  WHERE DATE = '2020-01-05';
+ALTER TABLE COPY_DB.PUBLIC.ORDERS_CACHING CLUSTER BY ( DATE );
+-- Ideally we have wait for about 2 - 3 hours for the table to get clustered. 
+-- Clustering happens in the back end using snowflake warehouses
+SELECT * FROM COPY_DB.PUBLIC.ORDERS_CACHING  WHERE DATE = '2020-01-05';
+/*
+This result was not after 2 - 3 hours. It was just after creating the cluster key
+Query Duration	714ms
+Rows	97,763
+End time	Tue Oct 10 2023
+Scanned	1.1GB (100%)
+ */
 
 
 // Not ideal clustering & adding a different Cluster Key using function
 
-SELECT * FROM ORDERS_CACHING  WHERE MONTH(DATE)=11;
+SELECT * FROM COPY_DB.PUBLIC.ORDERS_CACHING  WHERE MONTH(DATE)=11;
+/*
+Query Duration	4s
+Rows	18,843,278
+End time	Tue Oct 10 2023
+Scanned	1.1GB (100%)
+ */
 
-ALTER TABLE ORDERS_CACHING CLUSTER BY ( MONTH(DATE) );
+ALTER TABLE COPY_DB.PUBLIC.ORDERS_CACHING CLUSTER BY ( MONTH(DATE) );
+-- Ideally we have wait for about 2 - 3 hours for the table to get clustered. 
+-- Clustering happens in the back end using snowflake warehouses
+SELECT * FROM COPY_DB.PUBLIC.ORDERS_CACHING  WHERE MONTH(DATE)=10;
+/*
+This result was not after 2 - 3 hours. It was just after creating the cluster key
+Query Duration	4s
+Rows	21,093,005
+End time	Tue Oct 10 2023
+Scanned	1.1GB (100%)
+ */
