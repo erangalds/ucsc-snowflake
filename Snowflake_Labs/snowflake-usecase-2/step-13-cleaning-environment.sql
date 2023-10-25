7722d577a2ae933dfd7f41844ac6ce22fa6afcdb
erangalds <erangalds@yahoo.com>
2023-10-25 16:15:06 +0530

    validated and commented on the step 2 define custom roles lab

--- Snowflake_Labs/snowflake-usecase-2/step-13-cleaning-environment.sql
+++ Snowflake_Labs/snowflake-usecase-2/step-13-cleaning-environment.sql
@@ -1,10 +1,33 @@
 -- Let's now clean the environment
-use role useradmin;
+use role securityadmin;
 use warehouse common_wh;
 select current_role(), current_warehouse();
+-- Checking for Users to clean
 -- Listing down all the users
 show users;
 -- Deleting the newly created users
 drop user kasun;
 drop user amali;
 drop user yometh;
+drop user nimasha;
+drop user dasun;
+drop user anjelo;
+drop user nalaka;
+drop user dewni;
+drop user wasana;
+drop user priyashan;
+
+-- Checking for Custom Roles to clean
+-- Listing down all the roles
+show roles;
+drop role sales_analyst;
+drop role sales_dba;
+drop role customer_support_analyst;
+drop role customer_support_dba;
+drop role datascientist;
+drop role marketing_analyst;
+drop role marketing_dba;
+
+
+
+
