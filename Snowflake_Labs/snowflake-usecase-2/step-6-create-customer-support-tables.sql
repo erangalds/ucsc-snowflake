206bf77a25653418fcf2615034b01ccfc7ff1b7c
erangalds <erangalds@yahoo.com>
2023-10-26 11:53:35 +0530

    validated and commented on the step 6 create customer support tables lab

--- Snowflake_Labs/snowflake-usecase-2/step-6-create-customer-support-tables.sql
+++ Snowflake_Labs/snowflake-usecase-2/step-6-create-customer-support-tables.sql
@@ -1,6 +1,8 @@
 /* Defining Customer Support Schema Tables */
 -- Login as CUSTOMER_SUPPORT_DBA and create the 
 -- Customer Support DBA - Nalaka
+use role customer_support_dba;
+use warehouse customer_support_wh;
 -- Verify that he can see the dev_maven_market.customer_support database and schema
 SHOW DATABASES;
 -- Selecting the dev_maven_market database
@@ -9,7 +11,7 @@ USE dev_maven_market;
 SHOW SCHEMAS;
 -- Selecting Sales Schema for Table Creation
 USE dev_maven_market.customer_support;
-
+select current_role(), current_warehouse(), current_database(), current_schema();
 -- Creating Tables
 -- Returns Table
 create or replace table dev_maven_market.customer_support.Returns(
