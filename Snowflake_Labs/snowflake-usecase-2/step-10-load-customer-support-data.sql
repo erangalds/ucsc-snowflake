96ff39763e89b418cdbf40928d2aafea4e7d485d
erangalds <erangalds@yahoo.com>
2023-10-26 12:54:24 +0530

    validated and commented on step 10 load customer support data lab

--- Snowflake_Labs/snowflake-usecase-2/step-10-load-customer-support-data.sql
+++ Snowflake_Labs/snowflake-usecase-2/step-10-load-customer-support-data.sql
@@ -1,7 +1,12 @@
 /* Loading Customer Support Data */
 -- Login as CUSTOMER_SUPPORT_DBA - Nalaka
 /* Copying the DATA FROM CSV FILES INTO target TABLES */
-
+use role customer_support_dba;
+use warehouse customer_support_wh;
+use schema dev_maven_market.customer_support;
+select current_role(), current_warehouse(), current_database(),current_schema();
+/* Copying the DATA FROM CSV FILES INTO target TABLES */
+list @dev_maven_manage.customer_support.maven_market_returns;
 /* Product Returns Data */
 copy into dev_maven_market.customer_support.Returns
 from @dev_maven_manage.customer_support.maven_market_returns/MavenMarket_Returns_1997-1998.csv
