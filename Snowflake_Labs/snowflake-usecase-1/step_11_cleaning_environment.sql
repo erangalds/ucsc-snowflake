fafd684bb4df141b883fc37c65e7e60663350a99
erangalds <erangalds@yahoo.com>
2023-11-04 11:24:34 +0530

    added a environment clean script

---
+++ Snowflake_Labs/snowflake-usecase-1/step_11_cleaning_environment.sql
@@ -0,0 +1,27 @@
+use role accountadmin;
+use warehouse compute_wh;
+drop database copy_db;
+drop database demo3a_db;
+drop database demo3b_db;
+drop database manage_db;
+drop database marketing_database;
+drop database finance_database;
+drop database temp_database;
+drop database maven_market;
+drop database maven_market_manage;
+drop database myfirst_db;
+drop database our_first_db;
+
+
+-- Dropping Users
+show users;
+drop user ds2;
+drop user finance_analyst_1;
+drop user finance_dba_1;
+drop user marketing_analyst_1;
+drop user marketing_dba_1;
+
+show warehouses;
+
+show stages;
+show storage integrations;
